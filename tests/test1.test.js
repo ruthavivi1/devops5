cb25ff670179efac9630e5bba7487d5601994b74
ruthavivi <73632861+ruthavivi@users.noreply.github.com>
2023-05-08 09:27:24 +0300

    test1

---
+++ tests/test1.test.js
@@ -0,0 +1,14 @@
+const request=require('supertest')
+const app=require('../server')
+
+describe('test suit 1:',()=>{
+    test("test 1:",async()=>{
+        const res =await request(app).get('/')
+        expect(res.statusCode).toEqual(200)
+    })
+
+    test2("test 1:",async()=>{
+        const res =await request(app).get('/1234')
+        expect(res.statusCode).toEqual(404)
+    })
+})
