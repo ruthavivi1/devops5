e0b461fe6cade998fca842ea1762f72e6e4139f8
ruthavivi <73632861+ruthavivi@users.noreply.github.com>
2023-05-15 17:47:14 +0300

    test

--- server.js
+++ server.js
@@ -1,47 +1,47 @@
 const express = require('express');
+const { Pool } = require('pg');
 
 const app = express();
-const bodyParser = require('body-parser');
-const db = require('./db');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.get('/register', (req, res) => {
-  db.query('SELECT * FROM students', (err, result) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('An error occurred while fetching data from the database');
-    } else {
-      res.send(result.rows);
-    }
-  });
+const pool = new Pool({
+  user: 'your-db-user',
+  host: 'localhost',
+  database: 'your-db-name',
+  password: 'your-db-password',
+  port: 5432, // default PostgreSQL port
 });
 
-app.post('/register', (req, res) => {
-  const {
-    name, exam1, exam2, exam3,
-  } = req.body;
-
-  // Validate exam scores
-  if (!Number.isInteger(exam1) || !Number.isInteger(exam2) || !Number.isInteger(exam3)) {
-    return res.status(400).send('Exam scores must be integers');
+// GET request to retrieve all registered students
+app.get('/register', async (req, res) => {
+  try {
+    const { rows } = await pool.query('SELECT * FROM students');
+    res.status(200).json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'An error occurred while fetching data from the database' });
   }
-
-  db.query(
-    'INSERT INTO students(name, exam1, exam2, exam3) VALUES($1, $2, $3, $4)',
-    [name, exam1, exam2, exam3],
-    (err, result) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('An error occurred while inserting data into the database');
-      } else {
-        res.send({ message: 'Data inserted successfully' });
-        
-      }
-    },
-  );
 });
 
-module.exports = app;
+// POST request to register a new student
+app.post('/register', async (req, res) => {
+  try {
+    const { name, exam1, exam2, exam3 } = req.body;
 
+    // Validate exam scores
+    if (!Number.isInteger(exam1) || !Number.isInteger(exam2) || !Number.isInteger(exam3)) {
+      return res.status(400).json({ message: 'Exam scores must be integers' });
+    }
+
+    await pool.query('INSERT INTO students (name, exam1, exam2, exam3) VALUES ($1, $2, $3, $4)', [name, exam1, exam2, exam3]);
+    res.status(201).json({ message: 'Student registered successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'An error occurred while inserting data into the database' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
